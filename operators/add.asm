c51643e23d762212620ee0915c8190bd76b8b875
Gérald DeGroote <grosged@users.noreply.github.com>
2016-08-11 16:53:55 +0200

    Update add.asm
    CPs replaced with RRAs (twice faster)

--- operators/add.asm
+++ operators/add.asm
@@ -1,23 +1,22 @@
 functionAdd:
 	ld a, b
-	cp typeNumber
-	jp z, AddNumberXXX
-	cp typeVariable
-	jp z, AddVariableXXX
-	cp typeFunction
-	jp z, AddFunctionXXX
+	rra				; b contain odd or even value? (test bit 0)
+	jp nc, AddVariableXXX		; if even, means b=4
+	rra				; test bit 1
+	jp c, AddFunctionXXX		; if set, means b=3
+	rra				; test bit 2
+	jp nc, AddNumberXXX		; if reset, means b=1
 AddChainXXX:
-	ld a, c
-	cp ChainFirst
-	jr z, AddChainFirstXXX
+	bit 0, c
+	jr nz, AddChainFirstXXX
 AddChainPushXXX:
 	ld a, d
-	cp typeNumber
-	jr z, AddChainPushNumber
-	cp typeVariable
-	jr z, AddChainPushVariable
-	cp typeFunction
-	jr z, AddChainPushFunction
+	rra
+	jr nc, AddChainPushVariable
+	rra
+	jr c, AddChainPushFunction
+	rra
+	jr nc, AddChainPushNumber
 AddChainPushChain:
 	ld a, 0C1h
 	call InsertA						; pop bc
@@ -40,26 +39,18 @@ AddChainPushFunction:
 	jp InsertA							; add a, b
 AddChainFirstXXX:
 	ld a, d
-	cp typeNumber
-	jr z, AddChainFirstNumber
-	cp typeFunction
-	jr z, AddChainFirstFunction
-AddChainFirstVariable:
-	ld a, 0DDh
-	call InsertA						; add a, (ix+*) (1)
-	ld a, 086h
-	call InsertA						; add a, (ix+*) (2)
-	ld a, e
-	jp InsertA							; add a, (ix+*) (3)
+	rra
+	jr nc,AddChainFirstVariable
+	rra
+	jr c, AddChainFirstFunction
 AddChainFirstNumber:
 	ld a, e
-	or a
-	ret z
-	cp 1
+	dec a
+	ret c
 	jr nz, +_
 	ld a, 03Ch
 	jp InsertA							; inc a
-_:	cp 255
+_:	cp 254
 	jr nz, +_
 	ld a, 03Dh
 	jp InsertA							; dec a
@@ -67,21 +58,21 @@ _:	ld a, 0C6h
 	call InsertA						; add a, **
 	ld a, e
 	jp InsertA							; add a, XX
-AddChainFirstFunction:
-	ld a, 047h
-	call InsertA						; ld b, a
+AddChainFirstVariable:
+	ld a, 0DDh
+	call InsertA						; add a, (ix+*) (1)
+	ld a, 086h
+	call InsertA						; add a, (ix+*) (2)
 	ld a, e
-	call GetFunction
-	ld a, 080h
-	jp InsertA							; add a, b
+	jp InsertA							; add a, (ix+*) (3)
 AddNumberXXX:
 	ld a, d
-	cp typeNumber
-	jr z, AddNumberNumber
-	cp typeVariable
-	jr z, AddNumberVariable
-	cp typeFunction
-	jr z, AddNumberFunction
+	rra
+	jr nc, AddNumberVariable
+	rra
+	jr c, AddNumberFunction
+	rra
+	jr nc, AddNumberNumber
 AddNumberChain:
 	ld e, c
 	jr AddChainFirstNumber
@@ -103,12 +94,12 @@ AddNumberFunction:
 	jr AddChainFirstNumber
 AddVariableXXX:
 	ld a, d
-	cp typeNumber
-	jr z, AddVariableNumber
-	cp typeVariable
-	jr z, AddVariableVariable
-	cp typeFunction
-	jr z, AddVariableFunction
+	rra
+	jr nc, AddVariableVariable
+	rra
+	jr c, AddVariableFunction
+	rra
+	jr nc, AddVariableNumber
 AddVariableChain:
 	ld e, c
 	jp AddChainFirstVariable
@@ -134,12 +125,19 @@ AddFunctionXXX:
 	ld c, e
 	ld e, a
 	ld a, d
-	cp typeNumber
-	jr z, AddNumberFunction
-	cp typeVariable
-	jr z, AddVariableFunction
-	cp typeChain
-	jp z, AddChainFirstFunction
+	rra
+	jr nc, AddVariableFunction
+	rra
+	jr c,AddFunctionFunction
+	rra
+	jr nc, AddNumberFunction
+AddChainFirstFunction:
+	ld a, 047h
+	call InsertA						; ld b, a
+	ld a, e
+	call GetFunction
+	ld a, 080h
+	jp InsertA							; add a, b
 AddFunctionFunction:
 	ld a, c
 	call GetFunction
