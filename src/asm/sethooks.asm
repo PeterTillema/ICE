47d3df960d865d136058f037b0e1416fee98b691
PeterTillema <petertillema@solcon.nl>
2018-07-26 11:10:56 +0200

    Update hooks submodule

--- src/asm/sethooks.asm
+++ src/asm/sethooks.asm
@@ -8,10 +8,11 @@ _SetHooks1:
 	pop	hl			; HL = data pointer
 	ld	iy, 0D00080h
 	call	00213CCh		; _SetGetKeyHook
-	ld	de, 688
+	ld	de, 693
 	add	hl, de
 	call	00213F8h		; _SetTokenHook
 	ld	de, 32
+	add	hl, de
 	call	00213C4h		; _SetCursorHook
 	push	hl
 	push	bc
